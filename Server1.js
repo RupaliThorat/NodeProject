209307f35fc374a59732f088375c86c55b298127
RupaliThorat <68444365+RupaliThorat@users.noreply.github.com>
2022-07-27 00:33:52 +0530

    Update Server1.js

--- Server1.js
+++ Server1.js
@@ -3,12 +3,12 @@ var app=express();
 
 var onData=function(rq,rs){
 
-var pro=[
-  {"id":23,"FirstName":"Rupali", "LastName":"Thorat"}
-  {"id":13,"FirstName":"Ajay", "LastName":"Mane"}
+  var pro=[
+  {"id":23,"FirstName":"Rupali", "LastName":"Thorat"},
+  {"id":13,"FirstName":"Ajay", "LastName":"Mane"},
   {"id":3,"FirstName":"Riya", "LastName":"Pawar"}
-];
-rs.send(pro);
+  ];
+  rs.send(pro);
  }
  app.get("/api/emp",onData);
 app.listen(3000);
